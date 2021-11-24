cf842643b4d85f4c6649e44314530a398c4f5dbf
sonu kumar <63550598+sonuku58@users.noreply.github.com>
2021-11-24 11:27:06 +0530

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using core3._1api.DTOs;
+using core3._1api.Model;
+using core3._1api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+namespace core3._1api.Controllers
+{
+    [ApiController]
+    [Route("api/student")]
+
+    public class StudentController : Controller
+    {
+        
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IStudentRepository studentRepository;
+
+        public StudentController(ApplicationDbContext context,IMapper mapper,IStudentRepository _studentRepository)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.studentRepository = _studentRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<StudentDTO>> GetStudent()
+        {
+            var student = studentRepository.GetAllStudent();
+
+            var studentList = mapper.Map<List<StudentDTO>>(student);
+
+            return studentList;
+        }
+
+        [HttpGet("{Id:int}", Name="getStudent")]
+        public  ActionResult<StudentDTO> GetStudentById(int Id)
+        {
+            var studentbyid = studentRepository.StudentById(Id);
+            var studentDTO = mapper.Map<StudentDTO>(studentbyid);
+            return studentDTO;
+        }
+
+     
+
+        [HttpPost]
+        public ActionResult PostStudent([FromBody] studentCreationDTO studentcreation)
+        {
+            var student = mapper.Map<Student>(studentcreation);
+            studentRepository.AddStudent(student);
+            //var studentDTO = mapper.Map<StudentDTO>(student);
+            return new CreatedAtRouteResult("getStudent", new { Id = student.Id }, student);
+        }
+
+        [HttpPut("{Id:int}")]
+        public ActionResult ModifyStudent(int Id,[FromBody] studentCreationDTO studentCreation )
+        {
+            var exist = context.Students.FirstOrDefault(s => s.Id == Id); 
+
+            if (exist == null)
+            {
+                return NotFound();
+            }
+            
+            var student = mapper.Map<Student>(studentCreation);
+
+            studentRepository.UpdateStudent(Id, student);
+            return Ok();
+
+            
+
+
+        }
+
+       
+
+        [HttpDelete("{Id:int}")]
+        public ActionResult DeleteStudent(int Id)
+        {
+            var exist = context.Students.FirstOrDefault(s => s.Id == Id); ;
+          
+            if (exist==null)
+            {
+                return NotFound();
+            }
+            studentRepository.RemoveStudent(exist);
+            return NoContent();
+        }
+
+
+
+
+    }
+}
---
+++ Services/IStudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using core3._1api.DTOs;
+using core3._1api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core3._1api.Services
+{
+     public interface IStudentRepository
+    {
+  
+        IEnumerable<Student> GetAllStudent();
+
+        Student StudentById(int Id);
+
+        void AddStudent(Student student);
+
+        void UpdateStudent(int Id,Student student);
+
+        void RemoveStudent( Student student);
+    }
+}
